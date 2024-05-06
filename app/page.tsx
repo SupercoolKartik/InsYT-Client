988cd617cae2edf40a3be875a0d2a601d8ea3342
Suraj <surajkartik71@gmail.com>
2024-05-06 16:12:00 +0530

    Removed api as the data will be fetched from insyt-server and cleaned the main page codes so itNow fetches data after sending the pId and renders it on the client side

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
 "use client";
 
-import { ChangeEvent, useState } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 export default function Home() {
+  const [inputValue, setInputValue] = useState("");
   const [url, setUrl] = useState("");
-  const [resp, setResp] = useState("");
+  const [responseData, setResponseData] = useState<ResponseData | null>(null);
+
+  // Define a type/interface for the response data
+  interface ResponseData {
+    playlist_name: string;
+    playlist_size: number;
+    playlist_length: number;
+  }
+  const server_url = process.env.NEXT_PUBLIC_SERVER_URL;
   const enteringLink = (e: ChangeEvent<HTMLInputElement>) => {
     setUrl(e.target.value);
   };
-  const linkCheck = async () => {
-    console.log(url);
+  const linkCheck = async (event: FormEvent) => {
+    event.preventDefault();
 
     //Extracting the pId from the link
     const regex = /(?:list=)(.*)/;
@@ -18,16 +27,15 @@ export default function Home() {
 
       try {
         const response = await fetch(
-          `http://localhost:3000/api/server?pId=${playlistId}`,
+          `${server_url}/getdetails?pId=${playlistId}`,
           {
             method: "GET",
             headers: { "Content-Type": "application/json" },
-            //body: JSON.stringify(data),
           }
         );
         if (response.ok) {
-          const responseBody = await response.text();
-          setResp(responseBody);
+          const responseBody = await response.json();
+          setResponseData(responseBody);
           console.log("Response:", responseBody);
         } else {
           console.error(
@@ -41,27 +49,28 @@ export default function Home() {
     } else {
       console.error("Playlist ID not found in the URL");
     }
+    setUrl("");
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <span className="text-xl font-serif text-red-500 font-bold">insYT</span>
-      <span>{resp}</span>
-      <div className="flex my-2">
+      <span>{responseData?.playlist_name}</span>
+      <span>{responseData?.playlist_size}</span>
+      <span>{responseData?.playlist_length}</span>
+      <form onSubmit={linkCheck} className="flex my-2">
         <input
           onChange={enteringLink}
+          value={url}
           name="pin"
           className="w-1/2 text-gray-700 bg-gray-200 border border-gray-300 rounded-l py-2 px-4 focus:outline-none"
           type="text"
           placeholder="Enter link here.."
         />
-        <button
-          onClick={linkCheck}
-          className="flex-shrink-0 md:ml-1 bg-red-400 text-white py-2 px-3 focus:outline-none hover:bg-red-600 rounded-r-md"
-        >
+        <button className="flex-shrink-0 md:ml-1 bg-red-400 text-white py-2 px-3 focus:outline-none hover:bg-red-600 rounded-r-md">
           Check Details
         </button>
-      </div>
+      </form>
     </main>
   );
 }
