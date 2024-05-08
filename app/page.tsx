fb56bd3690ac32a590072a79e4247c147c719ec4
Suraj <surajkartik71@gmail.com>
2024-05-08 23:34:40 +0530

    Introduced ShadCN/UI to the app and changed the classic form to Shadcn form

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,48 @@
 "use client";
 
 import { useState, ChangeEvent, useEffect, FormEvent } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 export default function Home() {
   const [inputValue, setInputValue] = useState("");
   const [url, setUrl] = useState("");
   //const [dur, setDur] = useState("");
   const [responseData, setResponseData] = useState<ResponseData | null>(null);
 
-  useEffect(() => {
-    //setDur("");
-  }, []);
+  const formSchema = z.object({
+    urlField: z.string().min(2),
+  });
 
-  // Define a type/interface for the response data
-  interface ResponseData {
-    playlist_name: string;
-    playlist_size: number;
-    playlist_length: number;
-  }
-  const server_url = process.env.NEXT_PUBLIC_SERVER_URL;
-  const enteringLink = (e: ChangeEvent<HTMLInputElement>) => {
-    setUrl(e.target.value);
-  };
-  const linkCheck = async (event: FormEvent) => {
-    event.preventDefault();
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      urlField: "",
+    },
+  });
 
+  // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    //console.log(values);
     //Extracting the pId from the link
+    const urll = values.urlField;
+    console.log(urll);
     const regex = /(?:list=)(.*)/;
-    const match = url.match(regex);
+    const match = urll.match(regex);
     if (match) {
       const playlistId = match[1];
 
@@ -59,6 +75,21 @@ export default function Home() {
       console.error("Playlist ID not found in the URL");
     }
     setUrl("");
+  }
+
+  // useEffect(() => {
+  //   //setDur("");
+  // }, []);
+
+  // Define a type/interface for the response data
+  interface ResponseData {
+    playlist_name: string;
+    playlist_size: number;
+    playlist_length: number;
+  }
+  const server_url = process.env.NEXT_PUBLIC_SERVER_URL;
+  const enteringLink = (e: ChangeEvent<HTMLInputElement>) => {
+    setUrl(e.target.value);
   };
 
   //FUNCTION TO FORMAT THE DURATION
@@ -91,47 +122,69 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <span className="text-xl font-serif text-red-500 font-bold">insYT</span>
-      <span>{responseData?.playlist_name}</span>
-      <span>{responseData?.playlist_size}</span>
+      <span>Title: {responseData?.playlist_name}</span>
+      <span>No of items: {responseData?.playlist_size}</span>
       {/* <span>
         {dur}
       </span> */}
-      <span>
-        {responseData?.playlist_length &&
-          formatTime(responseData.playlist_length, 1)}
-      </span>
-      <span>
-        {responseData?.playlist_length &&
-          formatTime(responseData.playlist_length, 1.25)}
-      </span>
-      <span>
-        {responseData?.playlist_length &&
-          formatTime(responseData.playlist_length, 1.5)}
-      </span>
-      <span>
-        {responseData?.playlist_length &&
-          formatTime(responseData.playlist_length, 1.75)}
-      </span>
-      <span>
-        {responseData?.playlist_length &&
-          formatTime(responseData.playlist_length, 2)}
-      </span>
-      <form onSubmit={linkCheck} className="flex my-2">
-        <input
-          onChange={enteringLink}
-          value={url}
-          name="pin"
-          className="w-1/2 text-gray-700 bg-gray-200 border border-gray-300 rounded-l py-2 px-4 focus:outline-none"
-          type="text"
-          placeholder="Enter link here.."
-        />
-        <button
-          type="submit"
-          className="flex-shrink-0 md:ml-1 bg-red-400 text-white py-2 px-3 focus:outline-none hover:bg-red-600 rounded-r-md"
+      <ul>
+        <li>
+          {responseData?.playlist_length &&
+            formatTime(responseData.playlist_length, 1)}
+        </li>
+        <li>
+          {responseData?.playlist_length &&
+            formatTime(responseData.playlist_length, 1.25)}
+        </li>
+        <li>
+          {responseData?.playlist_length &&
+            formatTime(responseData.playlist_length, 1.5)}
+        </li>
+        <li>
+          {responseData?.playlist_length &&
+            formatTime(responseData.playlist_length, 1.75)}
+        </li>
+        <li>
+          {responseData?.playlist_length &&
+            formatTime(responseData.playlist_length, 2)}
+        </li>
+      </ul>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit((data) => {
+            onSubmit(data);
+            form.reset(); // Reset the form after submission
+          })}
+          className="flex"
         >
-          Check Details
-        </button>
-      </form>
+          <FormField
+            control={form.control}
+            name="urlField"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Find length of any YouTube playlist:</FormLabel>
+                <div className="flex">
+                  <FormControl>
+                    <Input
+                      placeholder="www.youtube.com/playlist?list=example123..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <Button type="submit">Analyze</Button>
+                </div>
+
+                <FormDescription>
+                  You can enter a playlist link, playlist ID or even a video
+                  link from the playlist! This only works with playlists with
+                  upto 50 videos as of now.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
     </main>
   );
 }
