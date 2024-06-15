4d1a8651560d1f7224946d2ab1f9b94600e88e3b
Suraj <surajkartik71@gmail.com>
2024-06-15 19:52:24 +0530

    Added liks to LikedIn and Bye me a coffe accounts

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,10 +4,26 @@ const Footer = () => {
   return (
     <footer className="w-full flex flex-col sm:flex-row text-xs sm:font-semibold font-normal items-center justify-center  bg-gray-300  dark:bg-gray-950 py-1">
       <div>
-        Made by <Link href="#">Suraj Kr. Saw</Link> with ❤️ ,{" "}
+        Made by{" "}
+        <a
+          href="https://www.linkedin.com/in/suraj-kr-saw/"
+          target="_blank"
+          className="cursor-pointer text-purple-800 dark:text-purple-300"
+        >
+          Suraj Kr. Saw
+        </a>{" "}
+        with ❤️ ,{" "}
       </div>
       <div className="ms-1 me-2">
-        <Link href="#"> Buy me a Coffee</Link> may be? 🥺
+        <a
+          href="https://www.buymeacoffee.com/surajsaw"
+          target="_blank"
+          className="cursor-pointer text-purple-800 dark:text-purple-300"
+        >
+          {" "}
+          Buy me a coffee
+        </a>{" "}
+        may be? 🥺
       </div>
     </footer>
   );
